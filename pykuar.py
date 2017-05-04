61a070b9f346148faf95dfcd931701a8b7a762f5
Akram Shehadi <akram.shehadi@gmail.com>
2017-05-04 01:10:16 -0500

    Save QR codefiles to PNG

--- pykuar.py
+++ pykuar.py
@@ -1,7 +1,9 @@
 import qrcode
 import qrcode.image.svg
+from qrcode.image.pure import PymagingImage
 
-factory = qrcode.image.svg.SvgPathImage
+white_bg_factory 		= qrcode.image.svg.SvgPathFillImage
+transparent_bg_factory 	= qrcode.image.svg.SvgPathImage
 
 qr = qrcode.QRCode(
     version=1,
@@ -12,6 +14,6 @@ qr = qrcode.QRCode(
 qr.add_data('Some data')
 qr.make(fit=True)
 
-img = qr.make_image(image_factory = factory)
-
-img.save('qr.svg')
+img = qr.make_image(image_factory = PymagingImage)
+img_file = open('qr.png', 'wb')
+img.save(img_file)
